d8966ef8947ec98b4c36456b1ced8443f90a17e3
Paras <guptaparas039@gmail.com>
2021-06-27 13:15:49 +0530

    :gear: setup for installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="Tinge",
+    version="0.0.1",
+    packages=["tinge"],
+    url="",
+    license="MIT License",
+    author="Paras Gupta",
+    author_email="guptaparas039@gmail.com",
+    description="Colored text for terminal"
+)
