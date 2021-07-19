3a3b3aae8bd9f326eb8657109b0634ce8f6d40fe
Paras <guptaparas039@gmail.com>
2021-07-19 21:28:36 +0530

    :gear: Update setup, provied long_description

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
+import pathlib
 from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
 
 setup(
     name="Tinge",
-    version="0.0.1",
-    packages=["tinge"],
-    install_requires=["colorama"],
-    url="",
-    license="MIT License",
+    version="0.0.2",
+    description="Colored text for terminal",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/g-paras/tinge",
     author="Paras Gupta",
     author_email="guptaparas039@gmail.com",
-    description="Colored text for terminal",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["tinge"],
+    install_requires=["colorama"],
 )
