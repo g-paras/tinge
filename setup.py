d7367e0ae2cf1ce8ba8f36f22ffe0a4a94bcec7d
Paras Gupta <guptaparas039@gmail.com>
2021-07-19 23:53:48 +0530

    :gear: Add condition to Install Require

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 import pathlib
+import platform
 from setuptools import setup
 
 HERE = pathlib.Path(__file__).parent
 
 README = (HERE / "README.md").read_text()
 
+INSTALL_REQUIRES = []
+
+if platform.system() == "Windows":
+    INSTALL_REQUIRES.append("colorama")
 
 setup(
     name="Tinge",
@@ -16,11 +21,17 @@ setup(
     author="Paras Gupta",
     author_email="guptaparas039@gmail.com",
     license="MIT",
+    python_requires=">=3.5",
     classifiers=[
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "Topic :: Terminals",
+        "Topic :: Text Processing",
+        "Topic :: Utilities",
     ],
     packages=["tinge"],
-    install_requires=["colorama"],
+    install_requires=INSTALL_REQUIRES,
 )
