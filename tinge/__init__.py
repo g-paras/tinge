7a07c2dd6fa2827b25d3a39fd4d796dde50b28f0
Paras <guptaparas039@gmail.com>
2021-07-28 18:27:45 +0530

    :snake: Add new methods(cprint and hline)

--- tinge/__init__.py
+++ tinge/__init__.py
@@ -1,18 +1,30 @@
 """Entry point for import"""
 
-from .cprint import bold, colored, error, hline, info, italic, success, underline, warn
+from .cprint import (
+    bold,
+    colored,
+    cprint,
+    error,
+    hline,
+    info,
+    italic,
+    success,
+    underline,
+    warn,
+)
 from .utils import InvalidColorError
 
 __version__ = "0.0.5"
 __all__ = [
-    "colored",
-    "warn",
-    "error",
-    "info",
-    "success",
     "bold",
-    "underline",
-    "italic",
+    "colored",
+    "cprint",
+    "error",
     "hline",
+    "info",
+    "italic",
+    "success",
+    "underline",
+    "warn",
     "InvalidColorError",
 ]
